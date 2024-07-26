ce7893b83e56d03621861f2533e59f378a7508ca
Bodegram <84967903+devbodegram@users.noreply.github.com>
2024-07-26 18:28:08 +0100

    added api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import Axios  from "axios";
 
-const baseURL = 'http://127.0.0.1:5001/api'
+const baseURL = 'https://campus-8zi8.onrender.com/api'
 const api = Axios.create({
     baseURL,
     headers:{
